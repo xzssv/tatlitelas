dddf603a50ed0071a2433369219999e4038af65c
xzssv <aydkarakaya@live.com>
2024-08-13 13:55:02 +0300

    galeri güncel

--- tatlitelas/src/app/pages/gallery/gallery/gallery.component.ts
+++ tatlitelas/src/app/pages/gallery/gallery/gallery.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 
 interface Photo {
@@ -7,7 +7,6 @@ interface Photo {
   name: string;
   url: string;
   date: Date;
-  aspectRatio?: number;
 }
 
 @Component({
@@ -19,37 +18,21 @@ interface Photo {
 })
 export class GalleryComponent implements OnInit {
   photos: Photo[] = [];
-  widePhotos: Photo[] = [];
-  tallPhotos: Photo[] = [];
   selectedPhoto: Photo | null = null;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: any) { }
 
   ngOnInit() {
-    this.loadPhotos();
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadPhotos();
+    }
   }
 
   loadPhotos() {
     const savedPhotos = localStorage.getItem('photos');
     if (savedPhotos) {
       this.photos = JSON.parse(savedPhotos);
-      this.categorizePhotos();
     }
-  }
-
-  categorizePhotos() {
-    this.photos.forEach(photo => {
-      const img = new Image();
-      img.onload = () => {
-        photo.aspectRatio = img.width / img.height;
-        if (photo.aspectRatio > 1) {
-          this.widePhotos.push(photo);
-        } else {
-          this.tallPhotos.push(photo);
-        }
-      };
-      img.src = photo.url;
-    });
   }
 
   goBack() {
@@ -62,17 +45,5 @@ export class GalleryComponent implements OnInit {
 
   closeLightbox() {
     this.selectedPhoto = null;
-  }
-
-  viewAllWide() {
-    // Tüm yatay fotoğrafları gösterme işlemi
-    console.log("Tüm yatay fotoğraflar gösteriliyor");
-    // Burada tüm fotoğrafları gösteren bir modal veya yeni bir sayfa açabilirsiniz
-  }
-
-  viewAllTall() {
-    // Tüm dikey fotoğrafları gösterme işlemi
-    console.log("Tüm dikey fotoğraflar gösteriliyor");
-    // Burada tüm fotoğrafları gösteren bir modal veya yeni bir sayfa açabilirsiniz
   }
 }
